6fa30eda081dd3beaca2a8ed0445bbc5ea972c0c
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-28 11:27:37 +0500

    Update BlockMixin.java

--- src/main/java/feather/finch/mixin/BlockMixin.java
+++ src/main/java/feather/finch/mixin/BlockMixin.java
@@ -1,5 +1,6 @@
 package feather.finch.mixin;
 
+import feather.finch.block.FoldAnchorBlock; // ПРОВЕРЬ ЭТОТ ИМПОРТ!
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
@@ -11,9 +12,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Block.class)
 public class BlockMixin {
+    // Добавлен remap = true и проверь, совпадает ли сигнатура метода с твоей версией MC
     @Inject(method = "isTranslucent", at = @At("HEAD"), cancellable = true)
     private void featherFinch$anchorIsTranslucent(BlockState state, BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        if (state.getBlock() instanceof FoldAnchorBlock) {
+        // Обязательно убедись, что FoldAnchorBlock импортирован выше!
+        if ((Object)state.getBlock() instanceof FoldAnchorBlock) {
             cir.setReturnValue(true);
         }
     }
